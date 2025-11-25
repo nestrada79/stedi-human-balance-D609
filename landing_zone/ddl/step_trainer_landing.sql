ec93f8778372c7019c60f1f8a390ee354fc296c1
Natasha Estrada <nestra5@wgu.edu>
2025-11-25 12:54:06 -0500

    Renaming some files and moving for better repo structure

--- landing_zone/ddl/step_trainer_landing.sql
+++
@@ -1,8 +0,0 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS step_trainer_landing (
-    sensorReadingTime string,
-    serialNumber string,
-    distanceFromObject int
-)
-ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
-LOCATION 's3://stedi-d609-ne/step_trainer_landing/'
-TBLPROPERTIES ('classification'='json');
